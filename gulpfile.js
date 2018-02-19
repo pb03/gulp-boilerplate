0dab0d280627ea8c8a4f8d3a8a743ac3d5e92f35
Prasanjit Singh <prasanjit.singh@kayako.com>
2018-02-20 01:19:20 +0530

    Remove icons support

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,10 @@ var gulp = require('gulp'),
   concat       = require('gulp-concat'),
   sass         = require('gulp-sass'),
   cleanCSS     = require('gulp-clean-css'),
-  uglify       = require('gulp-uglify'),
-  svgmin       = require('gulp-svgmin'),
-  svgSprite    = require('gulp-svg-sprites'),
+  uglify       = require('gulp-uglify-es').default,
   browserSync  = require('browser-sync').create(),
 
-  // Define paths
+  // Paths for source files
   paths = {
     html: [
       '*.html'
@@ -18,8 +16,8 @@ var gulp = require('gulp'),
     scss: [
       // Include settings - order is important
       'assets/scss/settings/vars.scss',
-      'assets/scss/settings/mixins.scss',
-      'assets/scss/settings/sprite.scss',
+      'assets/scss/settings/reset.scss',
+
       // Include all other styles
       'assets/scss/ui/*.scss',
       'assets/scss/ui/**/*.scss',
@@ -28,25 +26,22 @@ var gulp = require('gulp'),
     js: [
       'assets/js/*.js',
       'assets/js/**/*.js',
-    ],
-    icons: [
-      'assets/icons/*.svg'
     ]
   };
 
 /**
  * Concatinate and compress SCSS
  */
-gulp.task('sass', ['sprite'], function() {
+gulp.task('sass', function() {
   return gulp.src(paths.scss)
-    .pipe(concat('app.css'))
+    .pipe(concat('main.css'))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css/'));
+    .pipe(gulp.dest('dist/'));
 });
 
 /**
@@ -54,30 +49,9 @@ gulp.task('sass', ['sprite'], function() {
  */
 gulp.task('js', function() {
   return gulp.src(paths.js)
-    .pipe(concat('app.js'))
+    .pipe(concat('main.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
-
-/**
- * Create sprite of SVG icons
- */
-gulp.task('sprite', function() {
-  return gulp.src(paths.icons)
-    // minify svg files
-    .pipe(svgmin())
-    // create sprite
-    .pipe(svgSprite({
-      cssFile: "../../assets/scss/settings/sprite.scss",
-      padding: 0,
-      preview: false,
-      selector: "icon-%f",
-      svg: {
-        sprite: "sprite.svg"
-      },
-      svgPath: "../img/%f"
-    }))
-    .pipe(gulp.dest("dist/img/"));
+    .pipe(gulp.dest('dist/'));
 });
 
 /**
@@ -87,7 +61,7 @@ gulp.task('serve', function() {
     // Server path
     browserSync.init({ server: "./" });
     // Reload browser when a dist file is updated
-    gulp.watch([paths.html, 'dist/css/app.css', 'dist/js/app.js']).on('change', browserSync.reload);
+    gulp.watch([paths.html, 'dist/main.css', 'dist/main.js']).on('change', browserSync.reload);
 });
 
 /**
@@ -96,5 +70,4 @@ gulp.task('serve', function() {
 gulp.task('default', ['sass', 'js', 'serve'], function() {
   gulp.watch(paths.scss, ['sass']);
   gulp.watch(paths.js, ['js']);
-  gulp.watch(paths.icons, ['sass']);
 });
